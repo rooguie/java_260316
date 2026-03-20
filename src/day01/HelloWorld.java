91b792880e4befb39f7e8cc8085b5b20ae88c5f8
rooguie <rooguie@naver.com>
2026-03-20 19:02:14 +0900

    day01

---
+++ src/day01/HelloWorld.java
@@ -0,0 +1,28 @@
+package day01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// 실행 메서드
+		//주석 (코드 제외되는 공간): 한줄 주석
+		/*여러줄
+		 * 주석
+		 * 
+		 * console : 출력 공간
+		 * String : 문자열을 처리하는 클래스 -->"문자열"
+		 * 
+		 */
+		System.out.println("Hello world~~!"); //출력 방법
+		System.out.println("asas");//java의 끝은 ; 마무리
+		
+		byte b = 30;
+		
+		//반드시 잦ㄱ은 따옴표를 사용하여 묶어줘야함
+		char ch='a';
+		
+		//원래 클래스는 정의하는 방법이 다르지만,
+		//String만은 예외로 자료형처럼 정의가능
+		String str="Hello World";
+	}
+
+}
